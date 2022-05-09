ba6142211a40c38803b9e8846f34c512bea4feb4
glukhovsa <stepa.gl@yandex.ru>
2022-05-09 12:34:12 +0300

    First Comment

--- Program.c
+++ Program.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
+
+`#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #define SWAP(type, array, x, y) type z=array[x]; array[x]=array[y]; array[y]=z;
+#define MIN_SIZE 5000;
+#define MAX_SIZE 35000;
+#define STEP 50;
 
-void generation(int *array1, int *array2, int len){
+void generation(int *array1, int *array2, int len){ //функция, заполняющая два переданных массива, рандомными, но одинаковыми числами
     int i, g;
     for(i=0; i<len; i++){
         g=rand()%201;
@@ -13,14 +17,13 @@ void generation(int *array1, int *array2, int len){
     }
 }
 
-float bubbleSort(int *array, int size){
-    int start = clock();
+float bubbleSort(int *array, int size){ //функция сортировки пузырьком, принимает указатель на массив, а возвращает время работы алгоритма
+    int start = clock(); //объявление переменной start(хранит количество тиков прошедших с начала запуска программы)
     for(int i=0; i<size-1; i++)
         for(int j=size-1; j>i; j--)
-            if(array[j-1]>array[j]){
+            if(array[j-1]>array[j])
                 SWAP(int, array, j-1, j);
-            }
-    return ((clock()-start)*1.0)/CLOCKS_PER_SEC;
+    return ((clock()-start)*1.0)/CLOCKS_PER_SEC; //возвращаем разницу между количеством тиков, прошедших с начала программы, и значением переменной start, переводя значение в секунды
 }
 
 void heapify(int *arr, int n, int i) {
@@ -53,19 +56,19 @@ float heapSort(int *arr, int n) {
 
 int main(){
     FILE *file;
-    file=fopen("file.txt", "w");
+    file=fopen("file.txt", "w"); //открытие файла для записи в него
     int length;
     float timeBubble, timeHeap;
-    for(length=5000; length<=35000; length+=50){
-        int *massBubble=malloc(sizeof(int)*length);
-        int *massHeap=malloc(sizeof(int)*length);
+    for(length=MIN_SIZE; length<=MAX_SIZE; length+=STEP){ //рассматривание работу алгоритмов для массивов длиной от MIN_SIZE до MAX_SIZE
+        int *massBubble=malloc(sizeof(int)*length); //выделение памяти под массив, который будет отсортирован сортировкой пузырьком
+        int *massHeap=malloc(sizeof(int)*length); //выделение памяти под массив, который будет отсортирован пиромидальной сортировкой
         generation(massBubble, massHeap, length);
-        timeBubble=bubbleSort(massBubble, length);
-        timeHeap=heapSort(massHeap, length);
-        fprintf(file, "%d %f %f\n", length, timeBubble, timeHeap);
+        timeBubble=bubbleSort(massBubble, length); //сортировка массива пузырьком, в переменную timeBubble заносится время работы алгоритма
+        timeHeap=heapSort(massHeap, length); //пиромидальная сортировка массива, в переменную timeBubble заносится время работы алгоритма
+        fprintf(file, "%d %f %f\n", length, timeBubble, timeHeap); //запись полученных данных в файл
         printf("%d\n", length);
-        free(massBubble);
-        free(massHeap);
+        free(massBubble); //высвобождение выделенной для массива памяти
+        free(massHeap); //высвобождение выделенной для массива памяти
     }
-    fclose(file);
+    fclose(file); //закрытие файла
 }
